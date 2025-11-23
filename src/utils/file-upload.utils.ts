c55bde984577077e86d242f38b787e170b15e4ba
Ali Noori <matineali@gmail.com>
2025-11-23 23:45:53 +0330

    correct path for save files

--- src/utils/file-upload.utils.ts
+++ src/utils/file-upload.utils.ts
@@ -2,6 +2,26 @@
 
 import { diskStorage } from 'multer';
 import { v4 as uuid } from 'uuid';
+import { extname, join } from 'path';
+
+export const uploadOptions = (folder: string) => ({
+    storage: diskStorage({
+        destination: (_req, _file, cb) => {
+            // مسیر مطلق روی سرور
+            const uploadPath = join('/var/www/petoman/uploads', folder);
+            cb(null, uploadPath);
+        },
+        filename: (_req, file, cb) => {
+            const unique = uuid();
+            const ext = extname(file.originalname);
+            cb(null, `${unique}${ext}`);
+        },
+    }),
+});
+
+/*
+import { diskStorage } from 'multer';
+import { v4 as uuid } from 'uuid';
 import { extname } from 'path';
 
 export const uploadOptions = (folder: string) => ({
@@ -13,4 +33,4 @@ export const uploadOptions = (folder: string) => ({
             cb(null, `${unique}${ext}`);
         },
     }),
-});
+});*/
