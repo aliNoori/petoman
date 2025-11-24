cd38e46b1ebc9481c10c2da31121462c9b174a2a
Ali Noori <matineali@gmail.com>
2025-11-24 17:17:40 +0330

    add tag seeder

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -2,6 +2,7 @@ import { DataSource } from 'typeorm';
 import { Category} from "../shared/category/category.entity";
 import { CategoryTypeEntity} from "../shared/category/category-type.entity";
 import {Post} from '../modules/Danim/post/post.entity'
+import {Tag} from "../shared/tag/tag.entity";
 export const AppDataSource = new DataSource({
     type: 'postgres',
     host: 'localhost',
@@ -9,6 +10,6 @@ export const AppDataSource = new DataSource({
     username: 'postgres',
     password: 'ame@6558U',
     database: 'pet',
-    entities: [Category,Post, CategoryTypeEntity],
+    entities: [Category,Post, CategoryTypeEntity,Tag],
     synchronize: false,
 });
