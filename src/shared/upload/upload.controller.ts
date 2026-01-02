6982074cafe372472a37cd56ead675da555a233a
Ali Noori <matineali@gmail.com>
2026-01-02 20:31:44 +0330

    fixed merge chunks with ffmpeg

--- src/shared/upload/upload.controller.ts
+++ src/shared/upload/upload.controller.ts
@@ -41,18 +41,8 @@ export const uploadOptions = (folder: string) => ({
 @Controller('v1/uploads')
 export class UploadController {
     constructor(private readonly uploadService: UploadService) {
-        const folders = [
-            BASE_UPLOAD_PATH,
-            join(BASE_UPLOAD_PATH, 'images'),
-            join(BASE_UPLOAD_PATH, 'videos'),
-            join(BASE_UPLOAD_PATH, 'files'),
-            join(BASE_UPLOAD_PATH, 'chunks'),
-        ];
-
-        folders.forEach(dir => {
-            if (!fs.existsSync(dir)) {
-                fs.mkdirSync(dir, { recursive: true });
-            }
+        ['uploads', 'uploads/images', 'uploads/videos', 'uploads/files'].forEach(p => {
+            if (!fs.existsSync(p)) fs.mkdirSync(p);
         });
     }
 
@@ -176,25 +166,12 @@ export class UploadController {
         const finalPath = join(videoDir, `${videoId}.mp4`);
         const listPath = join(chunkDir, `${videoId}.txt`);
 
-        console.log('baseUrl',baseUrl)
-        console.log('chunkDir',chunkDir)
-        console.log('videoDir',videoDir)
-        console.log('finalPath',finalPath)
-        console.log('listPath',listPath)
-
-
         // پیدا کردن همه chunkها
         const chunkFiles = fs.readdirSync(chunkDir)
-            .filter(f => f.startsWith(videoId))
+            //.filter(f => f.startsWith(videoId))
             .sort((a, b) => parseInt(a.split('-')[1]) - parseInt(b.split('-')[1]));
-        console.log('chunkFilesbif',chunkFiles)
+
         if (chunkFiles.length === 0) {
-            console.log('chunkFilesaif',chunkFiles)
-            console.log('baseUrl',baseUrl)
-            console.log('chunkDir',chunkDir)
-            console.log('videoDir',videoDir)
-            console.log('finalPath',finalPath)
-            console.log('listPath',listPath)
             throw new Error('هیچ چانکی پیدا نشد');
         }
 
